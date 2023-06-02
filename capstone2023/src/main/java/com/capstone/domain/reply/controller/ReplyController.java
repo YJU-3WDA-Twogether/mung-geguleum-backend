28c825ca73dac70c9c35575d86292f260cbf3fb2
Jongchan Jeon <103109563+JongchanJeon@users.noreply.github.com>
2023-06-02 17:28:37 +0900

    Revert "Jongchan"

--- capstone2023/src/main/java/com/capstone/domain/reply/controller/ReplyController.java
+++ capstone2023/src/main/java/com/capstone/domain/reply/controller/ReplyController.java
@@ -1,6 +1,6 @@
 package com.capstone.domain.reply.controller;
 
-import com.capstone.domain.reply.dto.ReplyRequest;
+import com.capstone.domain.reply.dto.ReplyDTO;
 import com.capstone.domain.reply.service.ReplyService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,23 +16,23 @@ public class ReplyController {
     private final ReplyService replyService;
 
     @PostMapping("/create")  // 댓글 작성
-    public ResponseEntity<Boolean> replyCreate(@Valid @RequestBody ReplyRequest replyDTO) {
-        System.out.println("유저 번호 : " + replyDTO.getUno() + "번 님이 '" + replyDTO.getReply()
+    public ResponseEntity<Boolean> commentCreate(@Valid @RequestBody ReplyDTO replyDTO) {
+        System.out.println("유저 번호 : " + replyDTO.getUno() + "번 님이 '" + replyDTO.getComment()
                 + "'라는 댓글을 썼습니다 --->" + replyDTO.getPno() +"번 게시글");
         replyService.replyCreate(replyDTO);
         return ResponseEntity.ok(true);
     }
 
-    @DeleteMapping("/delete/{rno}")
-    public ResponseEntity<Boolean> replyDelete(@PathVariable Long rno) {
-        replyService.replyDelete(rno);
+    @DeleteMapping("/delete/{ano}")
+    public ResponseEntity<Boolean> commentDelete(@PathVariable Long ano) {
+        replyService.replyDelete(ano);
         return ResponseEntity.ok(true);
     }
 
-    @PutMapping("/update") // 댓글 수정
-    public ResponseEntity<Boolean> replyUpdate(@RequestBody ReplyRequest replyDTO) {
+    @PutMapping("/update/{ano}") // 댓글 수정
+    public ResponseEntity<Boolean> commentUpdate(@PathVariable Long ano, @RequestBody ReplyDTO replyDTO) {
+        replyDTO.setAno(ano);
         replyService.replyUpdate(replyDTO);
-        System.out.println("wefwef" + replyDTO.toString());
         return ResponseEntity.ok(true);
     }
 }
--- capstone2023/src/main/java/com/capstone/domain/reply/repository/ReplyRepository.java
+++ capstone2023/src/main/java/com/capstone/domain/reply/repository/ReplyRepository.java
@@ -1,16 +1,11 @@
 package com.capstone.domain.reply.repository;
 
-import com.capstone.domain.post.entity.Post;
 import com.capstone.domain.reply.entity.Reply;
-import com.capstone.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
-    Optional<Object> findByRno(Long cno);
-
-    List<Reply> findByPostPno(Long pno);
+    Optional<Object> findByAno(Long cno);
 }
